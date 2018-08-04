6756f66b10a8d294c0883b4c28d9f50d0f6042f2
Teresa6312 <cuishan1122@gmail.com>
2018-08-03 23:16:55 -0400

    modify all inclode blocks

--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('accounts/register/', views.RegisterView.as_view(), name = 'register'),
     path('accounts/colregister/', views.ColRegisterView.as_view(), name = 'colregister'),
 
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='main/login_new.html'), name = 'login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='main/login.html'), name = 'login'),
     # path('accounts/login/', views.LoginView.as_view, name = 'login'),
 
 
