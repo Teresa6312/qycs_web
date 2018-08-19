5d8eeacd4cc0b2d33367ec031b7d707e5e75e448
Teresa6312 <cuishan1122@gmail.com>
2018-08-19 16:27:01 -0400

    map and modal

--- main/admin.py
+++ main/admin.py
@@ -247,3 +247,13 @@ class FavoriteWebsiteAdmin(admin.ModelAdmin):
 	search_fields = ['web_name']
 
 admin.site.register(FavoriteWebsite, FavoriteWebsiteAdmin)
+
+
+
+from django_google_maps import widgets as map_widgets
+from django_google_maps import fields as map_fields
+
+class RentalAdmin(admin.ModelAdmin):
+	formfield_overrides = {
+		map_fields.AddressField: {'widget': map_widgets.GoogleMapsAddressWidget},
+	}
