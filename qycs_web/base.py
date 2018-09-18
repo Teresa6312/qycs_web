5e23e2601c181962b22e29af34f4b3ae9303f0a4
haidarzxc <haidar.a.alanbari@gmail.com>
2018-09-18 18:09:38 -0400

    no setting context processeor

--- qycs_web/base.py
+++ qycs_web/base.py
@@ -130,8 +130,6 @@ TEMPLATES = [
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
-                # cloudinary
-                'main.context_processors.consts',
 
                 # social media login
                 'social_django.context_processors.backends',
