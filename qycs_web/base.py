9caa7879047a12125f21526f3c6a73f7add29364
Teresa6312 <cuishan1122@gmail.com>
2018-10-04 11:51:51 -0400

    information style

--- qycs_web/base.py
+++ qycs_web/base.py
@@ -86,14 +86,15 @@ SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    # for translation after SessionMiddleware and before CommonMiddleware
+    'django.middleware.locale.LocaleMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    # for translation
-    'django.middleware.locale.LocaleMiddleware',
 
     # social media login
     'social_django.middleware.SocialAuthExceptionMiddleware',
