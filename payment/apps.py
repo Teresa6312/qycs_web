831645432bd0a699be4af039a3625b62ca8f569f
Teresa6312 <cuishan1122@gmail.com>
2018-10-29 03:04:16 -0400

    hook

--- payment/apps.py
+++ payment/apps.py
@@ -1,13 +1,14 @@
 from django.apps import AppConfig
 from django.db.models.signals import pre_save
+from .hooks import payment_paid
 # from .hooks import show_me_the_money
 
 class PaymentConfig(AppConfig):
     name = 'payment'
 
-    # def ready(self):
-    #     # importing model classes
-    #     from paypal.standard.ipn.models import PayPalIPN
-    #
-    #     # registering signals with the model's string label
-    #     pre_save.connect(receiver, sender='app_label.PayPalIPN')
+    def ready(self):
+        # importing model classes
+        from paypal.standard.ipn.models import PayPalIPN
+
+        # registering signals with the model's string label
+        pre_save.connect(payment_paid, sender='app_label.PayPalIPN')
--- payment/views.py
+++ payment/views.py
@@ -6,34 +6,38 @@ from main.models import OrderSet
 from django.views.decorators.csrf import csrf_exempt
 
 def payment_process(request):
-    order_set_id = request.session.get('order_set_id')
-    orderSet = get_object_or_404(OrderSet, id = order_set_id)
-    host = request.get_host()
-    # What you want the button to do.
-    if orderSet.coupon:
-        paypal_dict = {
-            "business" : settings.PAYPAL_RECEIVER_EMAIL,
-            "amount": (orderSet.total_amount + orderSet.insurance),
-            "currency_code": orderSet.currency,
-            "item_name": "{} packages".format(orderSet.service_set.all().count()),
-            "discount_rate": orderSet.coupon.discount,
-            "invoice": orderSet.id,
-            "notify_url": 'http://{}{}'.format(host, reverse('payment:process')),
-            "return": 'http://{}{}'.format(host, reverse('userpackage')),
-            "cancel_return": 'http://{}{}'.format(host, reverse('package_cart')),
-        }
-    else:
-        paypal_dict = {
-            "business" : settings.PAYPAL_RECEIVER_EMAIL,
-            "amount": orderSet.total_amount,
-            "currency_code": orderSet.currency,
-            "item_name": "{} package(s)/order(s)".format(orderSet.service_set.all().count()),
-            "invoice": orderSet.id,
-            "notify_url": 'http://{}{}'.format(host, reverse('payment:process')),
-            "return": 'http://{}{}'.format(host, reverse('userpackage')),
-            "cancel_return": 'http://{}{}'.format(host, reverse('packagecart')),
-        }
-    # Create the instance.
-    form = PayPalPaymentsForm(initial=paypal_dict)
-    return render(request, 'payment/process.html', {'orderSet': orderSet,
-                                                    'form' : form})
+	order_set_id = request.session.get('order_set_id')
+	orderSet = get_object_or_404(OrderSet, id = order_set_id)
+	host = request.get_host()
+	# What you want the button to do.
+	if orderSet.coupon:
+		paypal_dict = {
+			"business" : settings.PAYPAL_RECEIVER_EMAIL,
+			"amount": (orderSet.total_amount + orderSet.insurance),
+			"currency_code": orderSet.currency,
+			"item_name": "{} packages".format(orderSet.service_set.all().count()),
+			"discount_rate": orderSet.coupon.discount,
+			"invoice": orderSet.id,
+			"notify_url": request.build_absolute_uri(reverse('paypal-ipn')),
+			"return": 'http://{}{}'.format(host, reverse('userpackage')),
+			"cancel_return": 'http://{}{}'.format(host, reverse('package_cart')),
+		}
+	else:
+		paypal_dict = {
+			"business" : settings.PAYPAL_RECEIVER_EMAIL,
+			"amount": (orderSet.total_amount + orderSet.insurance),
+			"currency_code": orderSet.currency,
+			"item_name": "{} package(s)/order(s)".format(orderSet.service_set.all().count()),
+			"invoice": orderSet.id,
+			"notify_url": request.build_absolute_uri(reverse('paypal-ipn')),
+			"return": 'http://{}{}'.format(host, reverse('userpackage')),
+			"cancel_return": 'http://{}{}'.format(host, reverse('packagecart')),
+		}
+	# Create the instance.
+	form = PayPalPaymentsForm(initial=paypal_dict)
+	return render(request, 'payment/process.html', {'orderSet': orderSet,
+													'form' : form})
+
+def paypal_processed(request):
+	if request.POST:
+		print(request)
