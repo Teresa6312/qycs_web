3756987d051a5b7922bdd29342be6e8b63a648b9
Teresa6312 <cuishan1122@gmail.com>
2018-10-30 12:01:13 -0400

    test=false

--- qycs_web/settings.py
+++ qycs_web/settings.py
@@ -49,9 +49,9 @@ ALLOWED_HOSTS = ['myqycs.com',]
 DEBUG = False
 
 # paypal
-# PAYPAL_RECEIVER_EMAIL = os.environ.get('PAYPAL_RECEIVER_EMAIL', '')
-PAYPAL_RECEIVER_EMAIL = "myqycs-facilitator@gmail.com"
-PAYPAL_TEST = True
+PAYPAL_RECEIVER_EMAIL = os.environ.get('PAYPAL_RECEIVER_EMAIL', '')
+# PAYPAL_RECEIVER_EMAIL = "myqycs-facilitator@gmail.com"
+PAYPAL_TEST = False
 PAYPAL_PDT_TOKEN = os.environ.get('PAYPAL_PDT_TOKEN', '')
 
 # email
