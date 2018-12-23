f9c3564901727bc296fc52d43f6f7e3f53a21de8
Teresa6312 <cuishan1122@gmail.com>
2018-12-23 18:26:00 +0800

    tabs

--- payment/views.py
+++ payment/views.py
@@ -50,80 +50,80 @@ from paypal.utils import warn_untested
 
 
 def process_pdt(request):
-    """
-    Payment data transfer implementation:
-    https://developer.paypal.com/webapps/developer/docs/classic/products/payment-data-transfer/
+	"""
+	Payment data transfer implementation:
+	https://developer.paypal.com/webapps/developer/docs/classic/products/payment-data-transfer/
 
-    This function returns a tuple of (pdt_obj, failed)
-    pdt_obj is an object of type PayPalPDT
-    failed is a flag that is True if the input data didn't pass basic validation.
+	This function returns a tuple of (pdt_obj, failed)
+	pdt_obj is an object of type PayPalPDT
+	failed is a flag that is True if the input data didn't pass basic validation.
 
-    Note: even for failed=False You must still check the pdt_obj is not flagged i.e.
-    pdt_obj.flag == False
-    """
+	Note: even for failed=False You must still check the pdt_obj is not flagged i.e.
+	pdt_obj.flag == False
+	"""
 
-    pdt_obj = None
-    txn_id = request.GET.get('tx')
-    failed = False
-    if txn_id is not None:
-        # If an existing transaction with the id tx exists: use it
-        try:
-            pdt_obj = PayPalPDT.objects.get(txn_id=txn_id)
+	pdt_obj = None
+	txn_id = request.GET.get('tx')
+	failed = False
+	if txn_id is not None:
+		# If an existing transaction with the id tx exists: use it
+		try:
+			pdt_obj = PayPalPDT.objects.get(txn_id=txn_id)
 			print('----------------------pdt_obj------------1-------------')
-        except PayPalPDT.DoesNotExist:
-            # This is a new transaction so we continue processing PDT request
-            pass
+		except PayPalPDT.DoesNotExist:
+			# This is a new transaction so we continue processing PDT request
+			pass
 
-        if pdt_obj is None:
+		if pdt_obj is None:
 			print('----------------------pdt_obj---------2----------------')
-            form = PayPalPDTForm(request.GET)
-            if form.is_valid():
+			form = PayPalPDTForm(request.GET)
+			if form.is_valid():
 				print('----------------------pdt_obj-------------3------------')
-                try:
-                    pdt_obj = form.save(commit=False)
+				try:
+					pdt_obj = form.save(commit=False)
 					print('----------------------pdt_obj-------------4------------')
-                except Exception as e:
-                    warn_untested()
-                    error = repr(e)
-                    failed = True
-            else:
+				except Exception as e:
+					warn_untested()
+					error = repr(e)
+					failed = True
+			else:
 				print('----------------------pdt_obj-------------5------------')
-                warn_untested()
-                error = form.errors
-                failed = True
+				warn_untested()
+				error = form.errors
+				failed = True
 
-            if failed:
+			if failed:
 				print('----------------------pdt_obj-----------------6--------')
-                warn_untested()
-                pdt_obj = PayPalPDT()
-                pdt_obj.set_flag("Invalid form. %s" % error)
+				warn_untested()
+				pdt_obj = PayPalPDT()
+				pdt_obj.set_flag("Invalid form. %s" % error)
 
-            pdt_obj.initialize(request)
+			pdt_obj.initialize(request)
 
-            if not failed:
+			if not failed:
 				print('----------------------pdt_obj-------------7------------')
-                # The PDT object gets saved during verify
-                pdt_obj.verify()
+				# The PDT object gets saved during verify
+				pdt_obj.verify()
 			print('----------------------pdt_obj--------------------8-----')
 			print(pdt_obj)
-    else:
-        pass  # we ignore any PDT requests that don't have a transaction id
+	else:
+		pass  # we ignore any PDT requests that don't have a transaction id
 
-    return (pdt_obj, failed)
+	return (pdt_obj, failed)
 
 @require_GET
 def payment_processed(request):
-    pdt_obj, failed = process_pdt(request)
-    context = {"failed": failed, "pdt_obj": pdt_obj}
-    if not failed:
+	pdt_obj, failed = process_pdt(request)
+	context = {"failed": failed, "pdt_obj": pdt_obj}
+	if not failed:
 
-        # WARNING!
-        # Check that the receiver email is the same we previously
-        # set on the business field request. (The user could tamper
-        # with those fields on payment form before send it to PayPal)
+		# WARNING!
+		# Check that the receiver email is the same we previously
+		# set on the business field request. (The user could tamper
+		# with those fields on payment form before send it to PayPal)
 
-        if pdt_obj.receiver_email == settings.PAYPAL_RECEIVER_EMAIL:
+		if pdt_obj.receiver_email == settings.PAYPAL_RECEIVER_EMAIL:
 
-            print('yes')
-            return render(request, reverse('packages'), context)
-    return render(request, reverse('packagecart'), context)
+			print('yes')
+			return render(request, reverse('packages'), context)
+	return render(request, reverse('packagecart'), context)
