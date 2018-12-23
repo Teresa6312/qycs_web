a72e26d6fbade40e1ebdd4f15ae26bb73b5fa200
Teresa6312 <cuishan1122@gmail.com>
2018-12-23 15:16:14 +0800

    test hook

--- payment/hooks.py
+++ payment/hooks.py
@@ -12,11 +12,16 @@ def payment_paid(sender, **kwargs):
 		if ipn_obj.receiver_email != settings.PAYPAL_RECEIVER_EMAIL:
 			return
 
+		print('------------ipn_obj.mc_gross_x---------------------')
+		print(ipn_obj.mc_gross_x)
+		print(order.total_amount-order.get_total()[1])
+
 		if ipn_obj.mc_gross_x == order.total_amount-order.get_total()[1] and ipn_obj.mc_currency == order.currency:
 			no_rush_amount = 0
 # for sub packages
 			if order.service_set.all().count()>0:
 				user = order.service_set.first().user
+				print('------------ipn_obj.mc_gross_x------------1---------')
 				if order.coupon:
 					for pack in order.service_set.all():
 						package = Service.objects.get(id = pack.id)
@@ -29,6 +34,7 @@ def payment_paid(sender, **kwargs):
 						package.save()
 						if package.no_rush_request:
 							no_rush_amount = no_rush_amount + package.paid_amount
+					print('------------ipn_obj.mc_gross_x------------2---------')
 				else:
 					for pack in order.service_set.all():
 						package = Service.objects.get(id = pack.id)
@@ -36,15 +42,17 @@ def payment_paid(sender, **kwargs):
 						package.save()
 						if package.no_rush_request:
 							no_rush_amount = no_rush_amount + package.paid_amount
+					print('------------ipn_obj.mc_gross_x------------3---------')
 
 # for parent_package
 			count = order.parentpackage_set.all().count()
 			if count > 0:
 				user = order.parentpackage_set.first().service_set.first().user
+				print('------------ipn_obj.mc_gross_x------------4---------')
 				if order.coupon:
 					for parent_pack in order.parentpackage_set.all():
 						paid_amount = 0
-
+						print('------------ipn_obj.mc_gross_x------------5---------')
 						for pack in parent_pack.service_set.all():
 							package = Service.objects.get(id = pack.id)
 							if package.order and order.coupon.order:
@@ -61,6 +69,7 @@ def payment_paid(sender, **kwargs):
 						p = ParentPackage.objects.get(id = parent_pack.id)
 						p.paid_amount = paid_amount
 						p.save()
+					print('------------ipn_obj.mc_gross_x------------6---------')
 				else:
 					for parent_pack in order.parentpackage_set.all():
 						for pack in parent_pack.service_set.all():
@@ -73,6 +82,7 @@ def payment_paid(sender, **kwargs):
 						p = ParentPackage.objects.get(id = parent_pack.id)
 						p.paid_amount = parent_pack.package_amount
 						p.save()
+						print('------------ipn_obj.mc_gross_x------------7---------')
 
 			paid_user = User.objects.get(id = user.id)
 			if order.currency == 'USD':
@@ -84,9 +94,12 @@ def payment_paid(sender, **kwargs):
 				coup = Coupon.objects.get(id = order.coupon.id)
 				coup.used_times = coup.used_times+1
 				coup.save()
+			print('------------ipn_obj.mc_gross_x------------8---------')
 		else:
+			print('------------ipn_obj.mc_gross_x------------9---------')
 			return
 	else:
+		print('------------ipn_obj.mc_gross_x------------10---------')
 		return
 
 valid_ipn_received.connect(payment_paid)
